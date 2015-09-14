f0d77c06f28371fa9578a9c6293a3c2f0b9f655c
jfblanco <blanco.jose.francisco@gmail.com>
2015-09-13 21:24:21 -0300

    Se arreglo el bug de los menues

---
+++ app/scripts/controllers/menuprincipal.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name odontoMoronFrontendApp.controller:MenuprincipalCtrl
+ * @description
+ * # MenuprincipalCtrl
+ * Controller of the odontoMoronFrontendApp
+ */
+angular.module('odontoMoronFrontendApp')
+  .controller('MenuprincipalCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.pacienteMenu = false;
+    $scope.turnoMenu = false;
+    $scope.atencionMenu = false;
+    $scope.operatoriaMenu = false;
+
+    $scope.$on("userLoggedIn",function(event,args) {
+	    if(args.rol == "ROLE_ADMINISTRADOR")
+	    {
+	    	$scope.pacienteMenu = true;
+		    $scope.turnoMenu = true;
+		    $scope.atencionMenu = false;
+		    $scope.operatoriaMenu = false;
+	    }
+	    if(args.rol == "ROLE_USER")
+	    {
+	    	$scope.pacienteMenu = true;
+		    $scope.turnoMenu = true;
+		    $scope.atencionMenu = true;
+		    $scope.operatoriaMenu = true;
+	    }
+	    if(args.rol == "ROLE_ODONTOLOGO")
+	    {
+	    	$scope.pacienteMenu = false;
+		    $scope.turnoMenu = false;
+		    $scope.atencionMenu = true;
+		    $scope.operatoriaMenu = false;
+	    }
+	});
+
+	$scope.$on("userLoggedOff",function(event,args) {
+	    $scope.pacienteMenu = false;
+	    $scope.turnoMenu = false;
+	    $scope.atencionMenu = false;
+	    $scope.operatoriaMenu = false;
+	});
+  });
