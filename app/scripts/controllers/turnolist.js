8c7d0ecd18d13f9ff2a484743233964f4dd11f78
jfblanco <blanco.jose.francisco@gmail.com>
2015-09-27 18:53:14 -0300

    Se agregaron la lista de pacientes por odontologo a la vista

--- app/scripts/controllers/turnolist.js
+++ app/scripts/controllers/turnolist.js
@@ -8,10 +8,34 @@
  * Controller of the odontoMoronFrontendApp
  */
 angular.module('odontoMoronFrontendApp')
-  .controller('TurnolistCtrl', function ($scope) {
+  .controller('TurnolistCtrl',['$scope','UsuarioService','TurnoService', function ($scope,UsuarioService,TurnoService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.odontologoDTO = null;
+
+    $scope.odontologos = [];
+
+    UsuarioService.buscarOdontologo().$promise.then(
+      function(odontologos)
+      {
+        for(var i = 0; i < odontologos.length; i++)
+          $scope.odontologos.push(odontologos[i][0]);
+      });
+
+    $scope.buscarPacientesParaElDia = function()
+    {
+    	if($scope.odontologoDTO != null)
+    	{
+	    	TurnoService.buscarPorOdontologo({id: $scope.odontologoDTO.id}).$promise.then(
+	    	function(pacientes)
+	    	{
+	    		console.log(pacientes);
+	    	});
+	    }
+    }
+
+  }]);
--- app/scripts/services/turnoservice.js
+++ app/scripts/services/turnoservice.js
@@ -10,6 +10,8 @@
 angular.module('odontoMoronFrontendApp')
   .service('TurnoService', ['$resource', 'VaribleService', function ($resource,VaribleService) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    var service = $resource(VaribleService.url()+'/turnos/:id');
+    var service = $resource(VaribleService.url()+'/turnos/:id', {}, {
+        'buscarPorOdontologo' : { method: 'GET', isArray:true, url: VaribleService.url()+'/turnos/pacienteodontologo/:id'}
+      });
     return service;
   }]);
